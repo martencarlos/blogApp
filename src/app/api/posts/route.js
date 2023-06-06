d257cd37c9ceb86763052c5c1da51ba14698bd2d
Carlos Marten <martencarlos@gmail.com>
2023-06-06 08:43:54 +0200

    dashboard filter by username

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,16 +4,14 @@ import Post from "@/models/Post";
 
 export const GET = async (request) => {
 
-  // const url = new URL(request.url);
-  // console.log(url);
-  // const username = url.searchParams.get("username");
+  const url = new URL(request.url);
+  console.log(url);
+  const username = url.searchParams.get("username");
 
   try {
     await connect();
    
-    // const posts = await Post.find(username && { username });
-    const posts = await Post.find({});
-
+    const posts = await Post.find(username && { username });
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
   } catch (err) {
