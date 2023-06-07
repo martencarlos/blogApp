117743aa3c587d3e4061f29d6222cd9c9d818577
Carlos Marten <martencarlos@gmail.com>
2023-06-07 09:05:35 +0200

    landing page redesign for mobile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.landingPage}>
+        
         <div className={styles.imageContainer}>
           <Image
             className={styles.featureImage}
@@ -17,15 +18,17 @@ export default function Home() {
             priority
           />
         </div>
+
         <div className={styles.textContainer}>
           <h1 className={styles.title}>Build Your Blog.<br/>Share Your Voice.</h1>
-          <p className={styles.subTitle}>{"Welcome to our blog creation site, where your ideas take center stage. Start writing, engage readers, and make your mark in the digital world. It's time to unleash your creativity and share your unique story with the world."}</p>
+          <p className={styles.subTitle}>{"Welcome to our blog creation site, where your ideas take center stage. "}</p>
+          <p className={styles.subTitle}>{"Start writing, engage readers, and make your mark in the digital world. It's time to unleash your creativity and share your unique story with the world."}</p>
           <br />
           <Link className={styles.linkButton} href="/dashboard">
             {"Let's get started!"}
           </Link>
-          
         </div>
+
       </div>
     </main>
   );
