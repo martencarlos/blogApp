7c908631fd68a99b919e96c4e7316b709504ed23
Carlos Marten <martencarlos@gmail.com>
2023-06-17 04:43:01 +0200

    blog id route fix

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -2,6 +2,21 @@ import { NextResponse } from "next/server";
 import dbConnect from "../../../lib/dbConnect";
 import User from "@/models/User";
 
+export const GET = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await dbConnect();
+
+    const user = await User.findById(id);
+
+    
+    return new NextResponse(JSON.stringify(user), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
+
 export const POST = async (request) => {
   console.log("GET query with params");
 
